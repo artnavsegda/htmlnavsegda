15eaf44b9c3ee1ada9bdf85be75f8efda5e23169
Art Navsegda <artnavsegda@gmail.com>
2020-03-19 01:13:27 +0300

    Update demo.js

--- dom/demo.js
+++ dom/demo.js
@@ -2,3 +2,7 @@ document.getElementById("demo").innerHTML = "Hello World!";
 var btn = document.createElement("BUTTON");
 btn.innerHTML = "CLICK ME";
 document.body.appendChild(btn);
+
+//for(var key in obj){
+//   keys.push(key);
+//}
