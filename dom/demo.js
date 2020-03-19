51ced91b0c6e6a3d74da15defa22b9be1a9587ce
Art Navsegda <artnavsegda@gmail.com>
2020-03-19 10:04:28 +0300

    a

--- dom/demo.js
+++ dom/demo.js
@@ -3,6 +3,8 @@ var btn = document.createElement("BUTTON");
 btn.innerHTML = "CLICK ME";
 document.body.appendChild(btn);
 
+console.log(schema.properties);
+
 for(var key in schema.properties){
    console.log(key);
 }
--- dom/schema.js
+++ dom/schema.js
@@ -2,12 +2,15 @@ var schema = {
   "type": "object",
   "properties": {
   	"band": {
+      "type": "string",
       "enum": ["5Ghz"]
     },
     "country": {
+      "type": "string",
       "enum": ["russia"]
     },
     "channel": {
+      "type": "string",
       "enum": ["5"]
     }
   }
